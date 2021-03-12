a07c8ca151fa859efe5b7053e90b7d8c6b598eae
giotherobot <giotherobot@gmail.com>
2021-03-12 22:51:53 +0100

    Reformatted code
    Now it compiles...

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,13 @@
 
 //smoothin variables
 const int numReadings = 20;
-long readings1[numReadings];      // reading buffer
-int readIndex = 0;              // index of the current reading
-long total1 = 0;                  // running total
-long average1 = 0;                // average
-long readings2[numReadings];      // reading buffer
-long total2 = 0;                  // running total
-long average2 = 0;                // average
+long readings1[numReadings]; // reading buffer
+int readIndex = 0;			 // index of the current reading
+long total1 = 0;			 // running total
+long average1 = 0;			 // average
+long readings2[numReadings]; // reading buffer
+long total2 = 0;			 // running total
+long average2 = 0;			 // average
 
 //CPU clock in MHz
 int cpu_freq = 0;
@@ -32,118 +32,116 @@ volatile unsigned long cpuTimeRising = 0;
 volatile unsigned long cpuTimeFalling = 0;
 volatile unsigned long cpuTimePlaceholder = 0;
 
-
 //other variables
 unsigned long elapsedCpuTime = 0;
-float velocity=0;
+float velocity = 0;
 
+void IRAM_ATTR isrCHANGE();
+long ToF(int trigger1, int trigger2, int echo1);
 
-void IRAM_ATTR isrCHANGE() {
-  //gets cpu timing when echo pin changes logic state
+void setup()
+{
 
-  cpuTimePlaceholder = ESP.getCycleCount(); //get cpu time before evaluating if statement
+	//set input and output
+	pinMode(sensor1Trigger, OUTPUT);
+	pinMode(sensor2Trigger, OUTPUT);
+	pinMode(sensor1Echo, INPUT);
+	pinMode(sensor2Echo, INPUT);
 
-  //assign cpu time to the right variable for high and low cases
-  if (digitalRead(currentEchoPin) == 1) {
-    cpuTimeRising = cpuTimePlaceholder;
-  }
-  else {
-    cpuTimeFalling = cpuTimePlaceholder;
-  }
+	//enable serial communication
+	Serial.begin(250000);
+
+	//get cpu frequency
+	cpu_freq = ESP.getCpuFreqMHz();
+
+	// initialize all the readings to 0:
+	for (int thisReading = 0; thisReading < numReadings; thisReading++)
+	{
+		readings1[thisReading] = 0;
+	}
+	for (int thisReading = 0; thisReading < numReadings; thisReading++)
+	{
+		readings2[thisReading] = 0;
+	}
 }
 
+void loop()
+{
+	//read ToF and smooth the values
+	// subtract the last reading:
+	total1 = total1 - readings1[readIndex];
+	total2 = total2 - readings2[readIndex];
+	// read from the sensor:
+	readings1[readIndex] = (ToF(sensor1Trigger, sensor2Trigger, sensor1Echo));
+	readings2[readIndex] = (ToF(sensor1Trigger, sensor2Trigger, sensor2Echo));
+	// add the reading to the total:
+	total1 = total1 + readings1[readIndex];
+	total2 = total2 + readings2[readIndex];
+	// advance to the next position in the array:
+	readIndex = readIndex + 1;
 
+	// if we're at the end of the array...
+	if (readIndex >= numReadings)
+	{
+		// ...wrap around to the beginning:
+		readIndex = 0;
+	}
 
+	// calculate the average:
+	average1 = total1 / numReadings;
+	average2 = total2 / numReadings;
 
-void setup() {
+	velocity = (0.15 * 1e12 * ((1.0 / average1) - (1.0 / average2)));
 
-  //set input and output
-  pinMode(sensor1Trigger, OUTPUT);
-  pinMode(sensor2Trigger, OUTPUT);
-  pinMode(sensor1Echo, INPUT);
-  pinMode(sensor2Echo, INPUT);
+	// send it to the computer as ASCII digits
 
-  //enable serial communication
-  Serial.begin(250000);
-
-  //get cpu frequency
-  cpu_freq = ESP.getCpuFreqMHz();
-
-  // initialize all the readings to 0:
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings1[thisReading] = 0;
-  }
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings2[thisReading] = 0;
-  }
-
+	Serial.println(velocity, 5);
 }
 
-void loop() {
-  //read ToF and smooth the values
-  // subtract the last reading:
-  total1 = total1 - readings1[readIndex];
-  total2 = total2 - readings2[readIndex];
-  // read from the sensor:
-  readings1[readIndex] =   (ToF(sensor1Trigger, sensor2Trigger, sensor1Echo));
-  readings2[readIndex] =   (ToF(sensor1Trigger, sensor2Trigger, sensor2Echo));
-  // add the reading to the total:
-  total1 = total1 + readings1[readIndex];
-  total2 = total2 + readings2[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
+void IRAM_ATTR isrCHANGE()
+{
+	//gets cpu timing when echo pin changes logic state
 
-  // if we're at the end of the array...
-  if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
-  }
+	cpuTimePlaceholder = ESP.getCycleCount(); //get cpu time before evaluating if statement
 
-  // calculate the average:
-  average1 = total1 / numReadings;
-  average2 = total2 / numReadings;
-
-  velocity=(0.15*1e12*((1.0/average1)-(1.0/average2)));
-
-  // send it to the computer as ASCII digits
-  
-  Serial.println(velocity,5);
-
-
-
-
+	//assign cpu time to the right variable for high and low cases
+	if (digitalRead(currentEchoPin) == 1)
+	{
+		cpuTimeRising = cpuTimePlaceholder;
+	}
+	else
+	{
+		cpuTimeFalling = cpuTimePlaceholder;
+	}
 }
 
+long ToF(int trigger1, int trigger2, int echo1)
+{
+	//function: measure time of fligh from one sensor (label "1") to another (label "2")
+	// units: nanoseconds (1e-9s)
 
+	//enable interrupts for ToF calculation on echo1 pin and set current echo pin
+	currentEchoPin = echo1;
+	attachInterrupt(echo1, isrCHANGE, CHANGE);
 
-long ToF (int trigger1, int trigger2, int echo1) {
-  //function: measure time of fligh from one sensor (label "1") to another (label "2")
-  // units: nanoseconds (1e-9s)
+	//trigger both sensors
+	digitalWrite(trigger1, LOW); //reset trigger logic level before start
+	digitalWrite(trigger2, LOW);
+	delayMicroseconds(5);
+	digitalWrite(trigger1, HIGH); //send a 10uS trigger HIGH pulse
+	digitalWrite(trigger2, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigger1, LOW);
+	digitalWrite(trigger2, LOW);
 
-  //enable interrupts for ToF calculation on echo1 pin and set current echo pin
-  currentEchoPin = echo1;
-  attachInterrupt(echo1, isrCHANGE, CHANGE);
+	//wait while ISRs collect timing data, adjust delay to at least double of expected ToF
+	delay(20);
 
-  //trigger both sensors
-  digitalWrite(trigger1, LOW); //reset trigger logic level before start
-  digitalWrite(trigger2, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigger1, HIGH); //send a 10uS trigger HIGH pulse
-  digitalWrite(trigger2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger1, LOW);
-  digitalWrite(trigger2, LOW);
+	//disable interrupts
+	detachInterrupt(echo1);
 
-  //wait while ISRs collect timing data, adjust delay to at least double of expected ToF
-  delay(20);
+	//do some math
+	elapsedCpuTime = (cpuTimeFalling - cpuTimeRising) * (1000.0 / cpu_freq); //elapsed time in ns
 
-  //disable interrupts
-  detachInterrupt(echo1);
-
-  //do some math
-  elapsedCpuTime = (cpuTimeFalling - cpuTimeRising) * (1000.0 / cpu_freq); //elapsed time in ns
-
-
-
-  return elapsedCpuTime;
+	return elapsedCpuTime;
 }
